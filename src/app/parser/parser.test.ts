9538dbb0b1d5f33d7ab08c865b802de3ba0f051f
Long Nguyen <contact@longnvh.com>
2019-03-15 17:13:54 +0700

    Test case for parser.

---
+++ src/app/parser/parser.test.ts
@@ -0,0 +1,54 @@
+import { Lexer } from '../lexer/lexer';
+import { Parser } from './parser';
+import { LetStatement, Statement } from '../ast/ast';
+
+describe('parse program', () => {
+  const input = `
+    let x = 5;
+    let y = 10;
+    let foobar = 838383;
+  `;
+  const lexer = Lexer.fromInput(input);
+  const parser = Parser.new(lexer);
+
+  const program = parser.parseProgram();
+
+  test('program is not null', () => {
+    expect(program).not.toBe(null);
+  });
+
+  test('program statements can contain only 3 statements', () => {
+    expect(program.statements).toBe(3);
+  });
+
+  const tests = [
+    'x', 'y', 'foobar'
+  ];
+
+  tests.forEach((identifer, i) => {
+    const stmt = program.statements[i];
+    testLetStatement(stmt, identifer);
+  });
+
+});
+
+function testLetStatement (statement: Statement, name: string) {
+
+  test('statement.tokenLiteral is let', () => {
+    expect(statement.tokenLiteral()).toBe('let');
+  });
+
+  test('statement is LetStatement', () => {
+    expect(statement).toBeInstanceOf(LetStatement);
+  });
+
+  const letStatement: LetStatement = statement as LetStatement;
+  test(`letStatement.Name.Value is ${name}, current ${letStatement.name.value}`, () => {
+    expect(letStatement.name.value).toBe(name);
+  });
+
+  test(`name is ${name}, current ${letStatement.name.tokenLiteral()}`, () => {
+    expect(letStatement.name.tokenLiteral()).toBe(name);
+  });
+
+}
